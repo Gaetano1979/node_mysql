9fc8edc633c10d37a6baedb99177722d6bd437ed
Gaetano Sabino <gaetano.sabino@gmail.com>
2018-10-28 00:42:58 -0500

    hbs

--- 06-webServer/server.js
+++ 06-webServer/server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res) {
     // res.send('Hello World');
-    let salida = {
-        nombre: 'Gaetano',
-        apellido: 'Sabino',
-        email: 'gaetano.gmail',
-        url: req.url
-    };
+    // let salida = {
+    //     nombre: 'Gaetano',
+    //     apellido: 'Sabino',
+    //     email: 'gaetano.gmail',
+    //     url: req.url
+    // };
 
-    res.send(salida);
+    // res.send(salida);
+    res.render('home', {
+        nombre: 'Gaetano'
+    });
 });
 
 app.get('/data', (req, res) => {
